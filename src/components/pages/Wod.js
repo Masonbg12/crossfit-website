08fcfc25de227992554fbbb8258c6fe3f1822460
Masonbg12 <masonbg1206@gmail.com>
2025-07-31 13:48:23 -0500

    modified:   src/components/pages/Wod.js

--- src/components/pages/Wod.js
+++ src/components/pages/Wod.js
@@ -5,8 +5,9 @@ function Wod({ setIsLoading }) {
   const [workouts, setWorkouts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  
   // API URL from environment variables
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = "https://crossfit-website-n1pc.vercel.app/data";
   const mongoURL = `${API_URL}/data`;
 
   useEffect(() => {
